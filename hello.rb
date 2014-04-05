562a5a00792d0b4475ba143d4b9b72baddc2dbc0
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-05 14:42:22 +0900

    配列を用いる
    配列に適当な名前を格納し、
    入力された名前を番号順に表示するプログラムを書け

--- hello.rb
+++ hello.rb
@@ -1,3 +1,8 @@
-100.times do
-  puts "All work and no play makes Jack a dull boy."
+
+daraf = ["斎脇","大畠","寺西","高橋","種市"]
+
+i = 0
+while i < daraf.length do
+puts "#{i+1}番目の人の名前は#{daraf[i]}さんです。"
+i = i + 1
 end
