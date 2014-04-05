b462dfe0c4fba84892c8c3aaf29d2d0f3b6ed1a2
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-05 14:11:05 +0900

    次の文を実行するとどうなるか考察しなさい。
    if ("hello ruby" == "hello ruby") then
      puts "hello ruby."
    else
      puts "not hello ruby."
    end

--- hello.rb
+++ hello.rb
@@ -1,13 +1,5 @@
-x = 10
-y = 20
-z = 30
-volume = x * y * z
-area = 2 * ( x * y + y * z + z * x)
-puts "表面積は",area,"です。"
-puts "体積は",volume,"です。"
-#print "こうすることによってコメントアウトされる"また、複数コメントアウトする場合は下記
-=begin
-print "表面積は#{area}です。\n"
-print "体積は#{volume}です。\n"
-=end
-#複数コメントアウトするときの注意点は、=とその後に続く文字を離さないこと。
+if ("hello ruby" == "hello ruby") then
+  puts "hello ruby."
+else
+  puts "not hello ruby."
+end
