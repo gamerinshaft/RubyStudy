0fdf159b5ec6a085ad79a9669d9fa5f9c7e18d52
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-06 15:50:42 +0900

    配列とeach
    names = [“ほげ”, “ほげ”,”ほげ”]
    を自分の漢字一つ一つに上書きして格納し、
    自分の名前をコンソール上に表示せよ。

--- hello.rb
+++ hello.rb
@@ -1,8 +1,12 @@
 
-daraf = ["斎脇","大畠","寺西","高橋","種市"]
+names = ["ほげ","ほげ","ほげ"]
 
-i = 0
-while i < daraf.length do
-puts "#{i+1}番目の人の名前は#{daraf[i]}さんです。"
-i = i + 1
+names[0] = "種"
+names[1] = "市"
+names[2] = "隼"
+names[3] = "兵"
+
+names.each do |name|
+  print name
 end
+puts
