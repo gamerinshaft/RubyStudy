eb43579cb6921c91ae7626a3bb4eeea099f97055
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-07 17:09:46 +0900

    ()で囲めば解決しました
    ()で囲めばこんな書き方しなくてもすみました（泣）

--- grep.rb
+++ grep.rb
@@ -3,7 +3,7 @@ def grep(pattern, filename)
   file.each_line do |line|
     if pattern =~ line then
       print line
-      print "#{pattern =~ line}".to_i + 1,"行目\n"
+      print (pattern =~ line) + 1,"行目\n"
     end
   end
   file.close
