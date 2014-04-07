9bb7030d5876250c002b7be20138e1fc4f50e3ec
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-07 16:58:59 +0900

    別ファイルからメソッドを呼び出す
    別ファイルに記述したgrepメソッドをよびだして、
    コンソール上に表示するプログラムを書け

---
+++ grep.rb
@@ -0,0 +1,10 @@
+def grep(pattern, filename)
+  file = File.open(filename)
+  file.each_line do |line|
+    if pattern =~ line then
+      print line
+      print "#{pattern =~ line}".to_i + 1,"行目\n"
+    end
+  end
+  file.close
+end
--- hello.rb
+++ hello.rb
@@ -1,15 +1,5 @@
-def grep
-  pattern = Regexp.new(ARGV[0])
-  filename = ARGV[1]
-  file = File.open(filename)
+require "./grep"
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
 
-  file.each_line do |line|
-    if pattern =~ line then
-      print line
-      print "#{pattern =~ line}".to_i + 1,"行目\n"
-    end
-  end
-  file.close
-end
-
-grep()
+grep(pattern, filename)
