b3ab7968961bdd01b5aedba1bd4d54f13da64efa
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-07 14:36:17 +0900

    コンソールからの入力及び四則演算
    第一引数と第二引数で得た値を用いて、四則演算及びあまりを表示するプログラムを組め

--- hello.rb
+++ hello.rb
@@ -1,2 +1,9 @@
-name = ARGV[0]
-puts "Happy Birthday #{name} !"
+first = ARGV[0].to_i
+second = ARGV[1].to_i
+
+
+puts "#{first} + #{second} = #{first + second}"
+puts "#{first} - #{second} = #{first - second}"
+puts "#{first} * #{second} = #{first * second}"
+puts "#{first} / #{second} = #{first / second}"
+puts "#{first} % #{second} = #{first % second}"
