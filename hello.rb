9c98929a096105f2a9a1ed9d9252212f4f7c1724
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-08 01:32:32 +0900

    値の入れ替え
    a,b = 1,0
    puts a,b
    tmp = a
    a = b
    b = tmp
    puts a, b
    を簡略化せよ

--- hello.rb
+++ hello.rb
@@ -1,10 +1,4 @@
-a,b,c = 1, 2, 3, 4
-d,e,f = 1, 2
-g,h,*i = 1, 2, 3, 4
-j,*k,l = 1, 2, 3, 4, 5
-
-array = [a,b,c,d,e,f,g,h,i,j,k,l]
-array.each do |i|
-  p i
-end
-
+a,b = 1,0
+puts a, b
+a,b = b,a
+puts a, b
