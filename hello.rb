c696201d2d244e8a840b21b03d5126c2f09950c0
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-07 16:22:58 +0900

    grepプログラム
    第一引数にgrepしたい文字、第二引数にファイル名を書くと、そのファイル内にあるgrepしたい文字が書かれている行の文と、先頭から数えて何文字目
    かにあるのかを表示するコードを書け

--- hello.rb
+++ hello.rb
@@ -1,6 +1,11 @@
-filename = ARGV[0]
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
 file = File.open(filename)
+
 file.each_line do |line|
-  print line
+  if pattern =~ line then
+    print line
+    print "#{pattern =~ line}".to_i + 1,"行目\n"
+  end
 end
 file.close
