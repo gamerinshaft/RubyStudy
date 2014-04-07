3fb7127dd108c30b1f81fc94c7087acaff9ac76a
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-07 16:43:04 +0900

    自作メソッド
    先ほどのgrepプログラムをメソッド化して、
    呼び出してつかってみよ。

--- hello.rb
+++ hello.rb
@@ -1,11 +1,15 @@
-pattern = Regexp.new(ARGV[0])
-filename = ARGV[1]
-file = File.open(filename)
+def grep
+  pattern = Regexp.new(ARGV[0])
+  filename = ARGV[1]
+  file = File.open(filename)
 
-file.each_line do |line|
-  if pattern =~ line then
-    print line
-    print "#{pattern =~ line}".to_i + 1,"行目\n"
+  file.each_line do |line|
+    if pattern =~ line then
+      print line
+      print "#{pattern =~ line}".to_i + 1,"行目\n"
+    end
   end
+  file.close
 end
-file.close
+
+grep()
