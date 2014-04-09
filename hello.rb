719de61bc7da4e28fc6c3dd4c85ac1e86e1c1b7a
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-09 20:32:49 +0900

    クラスが継承しているクラス
    前回の問題で入力したオブジェクトクラスがどんなクラスを継承しているか確認するためのコードを書け。

--- hello.rb
+++ hello.rb
@@ -1,5 +1,7 @@
-items = [1, "hoge", nil, 3, "goo", "bar"]
+items = [1, "hoge", nil, 3.5, "goo", "bar"]
 items.each do |item|
+  puts item.class.ancestors
+
   case item
   when String
     puts "This is String."
