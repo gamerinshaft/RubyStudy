a5589dccecc8d5f7f3d0238410995914639417fd
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-12 18:57:29 +0900

    比較
    == 値が等しいかどうかの比較
    equal? オブジェクトが同じかどうかの比較
    eql? 数値が厳密に等しいかどうかの比較

--- hello.rb
+++ hello.rb
@@ -1,3 +1,11 @@
-profile = {:name=>"種市", :sex=>"男", :age=>18}
-puts profile[:age] + 1
-puts profile[:name] + "隼兵"
+hoge = "foo"
+bar = "f" + "o" + "o"
+p hoge == bar
+p hoge.eql?(bar)
+p hoge.equal?(bar)
+
+fuga = 1.0
+huga = 1
+p fuga == huga
+p fuga.eql?(huga)
+p fuga.equal?(huga)
