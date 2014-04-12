3acfea36c46d5b1818046428c441f23007c001e1
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-04-12 19:01:22 +0900

    times do
    コンソール上に
    一面の花
    と七回出力せよ

--- hello.rb
+++ hello.rb
@@ -1,11 +1,3 @@
-hoge = "foo"
-bar = "f" + "o" + "o"
-p hoge == bar
-p hoge.eql?(bar)
-p hoge.equal?(bar)
-
-fuga = 1.0
-huga = 1
-p fuga == huga
-p fuga.eql?(huga)
-p fuga.equal?(huga)
+7.times do
+  puts "一面の花"
+end
