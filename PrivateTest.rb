b450da4dd331ce1a0b9867fab4c1b49833873f53
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-05-20 12:53:15 +0900

    PrivateTest

---
+++ PrivateTest.rb
@@ -0,0 +1,16 @@
+class PrivateTest
+  def pub
+    puts "This is public instance_Method"
+  end
+
+  public :pub
+
+  def priv
+    puts "This is private instance_Method"
+  end
+
+  private :priv
+end
+
+PrivateTest.new.pub
+PrivateTest.new.priv
