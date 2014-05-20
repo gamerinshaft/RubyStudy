b89a835cfb56698f72ba5546214f30758012e63b
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-05-20 12:52:14 +0900

    ARGVの勉強

--- hello.rb
+++ hello.rb
@@ -1,7 +1,9 @@
-sum = 0
-for i in 1..10 do
-  sum += i
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
+file = File.open(filename)
+file.each_line do |line|
+  if pattern =~ line
+    print line
+  end
 end
-
-print sum
-
+file.close
